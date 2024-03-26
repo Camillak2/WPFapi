d8d4645776e91a23b1150b9da2b1ca996a6b94f4
Camillak2 <ksagdieva2@gmail.com>
2024-03-26 11:06:06 +0300

    Добавьте файлы проекта.

---
+++ WPFapi/NetManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WPFapi
+{
+    public static class NetManager
+    {
+        public static string URL = "https://localhost:44341/";
+        public static HttpClient httpClient = new HttpClient();   
+        
+        
+        
+        public static async Task<T> Get<T>(string controller)
+        {
+            var response = await httpClient.GetAsync(URL + controller);
+            var data = 
+        }
+
+
+
+    }
+
+ 
+}
